895ef2428b32ad3ee2ffb93f80cafae5e0a76591
Ricardo Conchas <ricardoconchas@gmail.com>
2021-09-21 11:27:32 -0500

    day8- prime numbers

---
+++ day8-functionsparameter.py
@@ -0,0 +1,41 @@
+#Write your code below this line 👇
+import math
+import os
+os.system('cls')
+
+
+# def paint_calc(height,width,cover):
+    
+#     number_of_cans = (test_h * test_w) / coverage
+#     print(f"You'll need {math.ceil(number_of_cans)} cans of paint")
+
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+#--------------------------------------- Prime numbers -------------------------------
+#Write your code below this line 👇
+
+# def prime_checker(number):
+#     counter = 1
+#     for i in range(1,number):
+#         if number % i == 0:
+#             counter +=1
+#     if counter > 2:
+#         print("It's not a prime number.")
+#     else:
+#         print("It's a prime number.")
+# def prime_checker2(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#     if is_prime:
+#         print("It's a prime number.")
+#     else:
+#         print("It's not a prime number.")
+
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
+#--------------------------------------- Caesar Cipher -------------------------------
