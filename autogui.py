dfb9e9dec00b4f913ede70ad6b259edbc2e7a58c
Ricardo Conchas <ricardoconchas@gmail.com>
2021-12-21 14:24:32 -0600

    autogui and other changes

---
+++ autogui.py
@@ -0,0 +1,39 @@
+import pyautogui as pg
+import time
+import random
+
+
+
+x_rand_position = random.randint(0,500)
+y_rand_position = random.randint(0,500)
+
+
+pg.FAILSAFE = False
+while True:
+    time.sleep(250)
+    for i in range(0,100):
+        pg.moveTo(x_rand_position, i * 5)
+    for i in range(0, 3):
+        pg.press('ctrl')
+        #pg.hotkey('alt','tab')
+
+# time.sleep(5)
+# pg.hotkey("win","r")
+# pg.typewrite("https://www.youtube.com/watch?v=t1NxNInQW9Q&ab_channel=EduardoRosas-FinanzasPersonales=45s\n")
+# time.sleep(2)
+# pg.typewrite("k")
+# pg.hotkey("win","r")
+# pg.typewrite("notepad\n")
+# time.sleep(1)
+# pg.typewrite("This was a triumph\n")
+# pg.hotkey("ctrl")
+# pg.hotkey("ctrl")
+# pg.hotkey("ctrl")
+# pg.hotkey("ctrl")
+# pg.hotkey("ctrl")
+# pg.hotkey("ctrl")
+# pg.hotkey("ctrl")
+# pg.hotkey("ctrl")
+# pg.sleep(3)
+
+
