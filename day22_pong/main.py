e6c248958df3e5d07f574ceed3480756f58cdac3
Ricardo <ricardoconchas@gmail.com>
2022-04-13 23:02:52 -0500

    add scoreboard file

--- day22_pong/main.py
+++ day22_pong/main.py
@@ -50,6 +50,14 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
         
+    # Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.bounce_x()
+        ball.home()
 
+    # Detect when L paddle misses
+    if ball.xcor() < -380:
+        ball.bounce_x()
+        ball.home()
 
 screen.exitonclick()
