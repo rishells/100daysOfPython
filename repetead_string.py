5cf1ccef7337d40c13b53890b896765b25d1cc77
Ricardo <ricardoconchas@gmail.com>
2022-04-25 16:21:00 -0500

    detect char in repeated string

--- repetead_string.py
+++ repetead_string.py
@@ -4,20 +4,37 @@ import random
 import re
 import sys
 
+
+s = "abcac"
+n = int(input("Number of characters to consider: "))
+
 def repeatedString():
     # Write your code here
     final_string = ""
     # if len(s) < 10:
     #     final_string
 
+def countA(s):
+    counter = 0
+    for letter in s:
+        if letter == 'a':
+            counter += 1
+    print(f"Total number of a's in the string are {counter}")
+
+def completeString(s):
+    newString = ""
+    if len(s) < n:
+        newString = s + s
+    return newString
+
 if __name__ == '__main__':
     
+    print(completeString(s))
+    # s = input()
 
-    s = input()
+    # n = int(input().strip())
 
-    n = int(input().strip())
-
-    #result = repeatedString(s, n)
-    result = "hello"
-    print(result)
+    # #result = repeatedString(s, n)
+    # result = "hello"
+    # print(result)
     
