b8df90f1183450ae86a9b7cdc68ccb703f923401
Ricardo <ricardoconchas@gmail.com>
2022-04-26 13:05:59 -0500

    Turtle crossing, oop, inheritance, method definition everywhere

--- day23_turtle_crossing/scoreboard.py
+++ day23_turtle_crossing/scoreboard.py
@@ -1,5 +1,28 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-290,250)
+        self.update_score_board()
+        
+    def update_score_board(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_score_board()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER! ", align="center", font=FONT)
+
+
+        
