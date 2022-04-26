e9af8f56a842598489c4be7642a04a67d36e5cc8
Ricardo <ricardoconchas@gmail.com>
2022-04-26 17:38:55 -0500

    testing fib algorithms

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,7 @@
+import time
+from tracemalloc import start
+start_time = time.time()
+
 # A naive recursive solution
 def fib(n):
     if n == 1 or n == 2:
@@ -9,6 +13,7 @@ def fib(n):
 
 
 # A memoized solution
+
 def fib_2(n, memo):
     if memo[n] is not None:
         return memo[n]
@@ -34,3 +39,19 @@ def fib_bottom_up(n):
     for i in range(3, n+1):
         bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
     return bottom_up[n]
+
+
+# 1 1 2 3 5 8 13 21 34
+
+
+
+def main():
+    memo = []
+    #print(fib(40)) # 42 sec --- naive recursive solution  --- Exponential time  ---
+    #print(fib_bottom_up(100)) # --- 0 sec bottom-up solution 
+    print(fib_2(100,memo))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == "__main__":
+    main()
