335769bb57be56f3769f66b908166fc98d6eb5ff
Ricardo <ricardoconchas@gmail.com>
2022-04-27 17:19:53 -0500

    day24 file mgmt

---
+++ day24_files/main.py
@@ -0,0 +1,12 @@
+#file = open("day24_files\my_file.txt") # Relative path
+# with open("G:\\My Drive\\rcdev\Python\\100daysOfPython\\day24_files\\my_file.txt") as file# Path
+#     contents = file.read()
+#     print(contents)
+#     #file.close() With "with" you can avoid using close
+
+
+with open("day24_files\my_file3.txt","a") as file:
+    for i in range(50000000):
+        file.write(f"\nNew text written from python line: {i}")
+
+
