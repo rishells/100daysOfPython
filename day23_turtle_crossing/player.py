913f661aa2b59bfab35c2bacadcab28c112202d4
Ricardo <ricardoconchas@gmail.com>
2022-04-28 16:42:12 -0500

    Revert " Turtle crossing, oop, inheritance, method definition everywhere"
    This reverts commit 92eac45f0818a3f6d2f2aa364488259e3b2cc1c6.

--- day23_turtle_crossing/player.py
+++ day23_turtle_crossing/player.py
@@ -1,28 +1,7 @@
-from turtle import Turtle
-
-
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.color("black")
-        self.shape("turtle")
-        self.seth(90)
-        self.penup()
-        self.go_to_start()
-
-    def go_up(self):
-        # new_y = self.ycor() + 20
-        # self.goto(self.xcor(),new_y)
-        self.forward(MOVE_DISTANCE)
-
-    def go_to_start(self):
-        self.goto(STARTING_POSITION)
-
-    def is_at_finish_line(self):
-        return self.ycor() > FINISH_LINE_Y
-            
+class Player:
+    pass
