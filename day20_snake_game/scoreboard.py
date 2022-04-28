2ecd82756e3442a5778fa069410d9a89199e9e95
Ricardo <ricardoconchas@gmail.com>
2022-04-28 16:33:18 -0500

    added score tracking in file to Snake game

--- day20_snake_game/scoreboard.py
+++ day20_snake_game/scoreboard.py
@@ -8,14 +8,17 @@ class Scoreboard(Turtle):
         super().__init__()
 
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_score()
         self.color("white")
         self.penup()
         self.goto(0,270)
         self.hideturtle()
         self.update_scoreboard()
         
-
+    def get_score(self):
+        with open("day20_snake_game\data.txt") as score_data:
+            score = score_data.read()
+            return int(score)
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}",False, align=ALIGNMENT, font=FONT)
@@ -29,6 +32,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("day20_snake_game\data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
