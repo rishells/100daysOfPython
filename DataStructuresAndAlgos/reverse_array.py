bd8e583b1d18b6f7463cbb2e2e07f80f5cd5c196
Ricardo Conchas~ <ricardoconchas@gmail.com>
2022-05-16 16:00:41 -0500

    reverse array mod

--- DataStructuresAndAlgos/reverse_array.py
+++ DataStructuresAndAlgos/reverse_array.py
@@ -16,19 +16,21 @@ import sys
 def reverse_array(len_arr, array_elements):
     container = []
     for i in range(len_arr,0,-1):
-        container.append(array_elements[i-1])
-    return container
+        #container.append(array_elements[i-1])
+        print(array_elements[i-1], end = ' ')
+    #return container
     # for item in array_elements:
     #     print(array_elements[item])
 
-def fix_output(container_list):
-    for new_container in container_list:
-        print(new_container, end = ' ')
+# def fix_output(container_list):
+#     for new_container in container_list:
+#         print(new_container, end = ' ')
 if __name__ == '__main__':
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
-    unformatted_lst = reverse_array(n,arr)
-    fix_output(unformatted_lst)
+    reverse_array(n,arr)
+    # unformatted_lst = reverse_array(n,arr)
+    # fix_output(unformatted_lst)
     # my_arr = {1,4,3,2}
     # print(len(my_arr))
