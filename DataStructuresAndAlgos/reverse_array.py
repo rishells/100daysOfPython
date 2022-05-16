6c0dc1176a253a7202e6b9cd2c02306115c7a568
Ricardo Conchas~ <ricardoconchas@gmail.com>
2022-05-16 13:29:21 -0500

    Reverse an array of integers

---
+++ DataStructuresAndAlgos/reverse_array.py
@@ -0,0 +1,34 @@
+# Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+# Sample Input
+
+# 4
+# 1 4 3 2
+# Sample Output
+
+# 2 3 4 1
+
+import math
+import os
+import random
+import re
+import sys
+
+def reverse_array(len_arr, array_elements):
+    container = []
+    for i in range(len_arr,0,-1):
+        container.append(array_elements[i-1])
+    return container
+    # for item in array_elements:
+    #     print(array_elements[item])
+
+def fix_output(container_list):
+    for new_container in container_list:
+        print(new_container, end = ' ')
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    unformatted_lst = reverse_array(n,arr)
+    fix_output(unformatted_lst)
+    # my_arr = {1,4,3,2}
+    # print(len(my_arr))
